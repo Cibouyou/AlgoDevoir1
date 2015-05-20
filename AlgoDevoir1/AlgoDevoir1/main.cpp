5ba20adb3aaf29ee021ba32b2906511030d18e1f
Nathan Le Ray <nathan@sxnlabs.com>
2015-05-20 14:03:21 -0400

    Initial Commit

---
+++ AlgoDevoir1/AlgoDevoir1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AlgoDevoir1
+//
+//  Created by Nathan Le Ray on 20/05/15.
+//  Copyright (c) 2015 Nathan Le Ray. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
