f99554970d01d5f0d38fd56d3300918a407d985c
Cibouyou <cibouyou@gmail.com>
2015-05-20 12:31:06 -0400

    Demarrage

---
+++ AlgoTest1/AlgoTest1.cpp
@@ -0,0 +1,49 @@
+// AlgoTest1.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+#include "stdio.h"
+#include <iostream> 
+#include <stdlib.h> 
+
+
+using namespace std;
+
+void strassen(){
+
+	cout << "A faire\n" << endl;
+}
+
+void tricompar(){
+
+	cout << "A faire\n" << endl;
+}
+
+void karatsuba(){
+
+	cout << "A faire\n" << endl;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	
+	
+	cout << "Algorithmique \n-- Travaux Pratiques 1 --\n" << endl;
+
+	cout << "--Algorithme de Strassen-- \n" << endl;
+
+	strassen();
+
+	cout << "--Comparaison des algorithmes de tri-- \n" << endl;
+
+	tricompar();
+
+	cout << "--Algorithme de Karatsuba--\n" << endl;
+
+	karatsuba();
+
+	system("pause");
+
+	return 0;
+}
+
