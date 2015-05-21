ca7350d8f99404195139285f5bd6c176b1ada4a7
Cibouyou <cibouyou@gmail.com>
2015-05-21 16:05:34 -0400

    Strassen

--- AlgoDevoir1/AlgoDevoir1/main.cpp
+++ AlgoDevoir1/AlgoDevoir1/main.cpp
@@ -28,27 +28,28 @@ using namespace std;
 void fill_array(int *a, int *b, int size) {
     for(int i = 0; i < size; i++) {
         a[i] = (rand() % 100) + 1;
-        
+
         // cout << a[i] << " ";
     }
-    
+
     b = a;
 }
 
 int main(int argc, const char * argv[]) {
-    
+
     int times[10][10];
-    
+    int dim = 0;
+
     int a1[SIZE], a2[2 * SIZE], a3[3 * SIZE], a4[4 * SIZE], a5[5 * SIZE];
     int a6[6 * SIZE], a7[7 * SIZE], a8[8 * SIZE], a9[9 * SIZE], a10[10 * SIZE];
-    
+
     int b1[SIZE], b2[2 * SIZE], b3[3 * SIZE], b4[4 * SIZE], b5[5 * SIZE];
     int b6[6 * SIZE], b7[7 * SIZE], b8[8 * SIZE], b9[9 * SIZE], b10[10 * SIZE];
-    
+
     clock_t t;
-    
+
     srand((unsigned) time(0));
-    
+
     fill_array(a1, b1, 1 * SIZE);
     fill_array(a2, b2, 2 * SIZE);
     fill_array(a3, b3, 3 * SIZE);
@@ -60,55 +61,48 @@ int main(int argc, const char * argv[]) {
     fill_array(a9, b9, 9 * SIZE);
     fill_array(a10, b10, 10 * SIZE);
 
-	int input1[4][4] = {
-			{ 13, 3, 6, 3 },
-			{ 6, 2, 8, 87 },
-			{ 37, 42, 57, 69 },
-			{ 42, 53, 64, 74 } };
 
-	int input2[4][4] = {
-			{ 75, 68, 5, 4 },
-			{ 64, 58, 47, 3 },
-			{ 55, 48, 34, 2 },
-			{ 45, 34, 27, 1 } };
-    
-    // Matrix inputM1(input1[0], 4);
-    // Matrix inputM2(input2[0], 4);
-    // cout << "Input A is:\n" << inputM1 << endl;
-    // cout << "Input B is:\n" << inputM2 << endl;
-    // Matrix result(4);
-    // result = inputM1 * inputM2;
-    // cout << "Result of A * B is:\n" << result << endl;
-    
-    
+    cout << "Choisissez la dimension des matrices :" << endl;
+    cin >> dim ;
+
+     Matrix m1(dim);
+     Matrix m2(dim);
+     cout << "Matrice A :\n" << m1 << endl;
+     cout << "Matrice B :\n" << m2 << endl;
+     Matrix result(4);
+     result = m1.strassen(m2);
+     cout << "Strassen de A * B:\n" << result << endl;
+
+
     // cout << endl << "\nMerge Sorted Array" << endl;
     // MergeSort *sorter = new MergeSort(a, b, SIZE);
     // sorter->print();
-    
+
     // cout << endl << "\nBubble Sorted Array" << endl;
     // BubbleSort *sorter = new BubbleSort(a, SIZE);
     // sorter->print();
-    
+
     // cout << endl << "\nQuick Sorted Array" << endl;
     // QuickSort *sorter = new QuickSort(a, SIZE);
     // sorter->print();
-    
+
     // This one doesn't always work, I can't figure out the reason.
     // cout << endl << "\nQuick Sorted Randomized Array" << endl;
     // QuickSortRand *sorter = new QuickSortRand(a, SIZE);
     // sorter->print();
-    
+
     // cout << endl << "\nInsertion Sorted Array" << endl;
     // InsertionSort *sorter = new InsertionSort(a, SIZE);
     // sorter->print();
-    
+
     // cout << endl << "\nHeap Sorted Array" << endl;
     // HeapSort *sorter = new HeapSort(a, SIZE);
     // sorter->print();
-    
+
     // cout << endl << "\nRadix Sorted Array" << endl;
     // RadixSort *sorter = new RadixSort(a, SIZE);
     // sorter->print();
-    
+    system("pause");
+
     return 0;
 }
--- AlgoDevoir1/AlgoDevoir1/matrix.h
+++ AlgoDevoir1/AlgoDevoir1/matrix.h
@@ -7,14 +7,27 @@
 #define MATRIX_H_
 
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include "time.h"
 
 class Matrix {
 	size_t width;
 	int *data;
 public:
 	Matrix(size_t _width) : width(_width) {
+
+    int random = 0;
+	srand(time(NULL));
+
 		this->data = new int[_width * _width];
+
+		for (size_t i = 0; i<width * width; ++i) {
+            random =rand()%50;
+			this->data[i] = random;
+		}
 	}
+
 
 	Matrix(int *data, size_t _width) : Matrix(_width) {
 		for (size_t i = 0; i<width * width; ++i) {
@@ -39,7 +52,6 @@ public:
 	int & operator[](const size_t i);
 	const int & operator[](const size_t i) const;
 
-private:
 	Matrix strassen(const Matrix &b) const;
 };
 
