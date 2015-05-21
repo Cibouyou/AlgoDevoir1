6a4a48e086065a06a8e283837b31d22026cc7d30
Nathan Le Ray <nathan@sxnlabs.com>
2015-05-21 17:28:30 -0400

    Syntax fixes

--- AlgoDevoir1/AlgoDevoir1/main.cpp
+++ AlgoDevoir1/AlgoDevoir1/main.cpp
@@ -28,161 +28,161 @@
 using namespace std;
 
 void reset_array(int *a, int *b, int size) {
-
+    
     for(int i = 0; i < size; i++) {
         a[i] = b[i];
     }
-
+    
 }
 
 void fill_array(int *a, int *b, int *c, int size) {
     for(int i = 0; i < size; i++) {
         a[i] = (rand() % 100) + 1;
-
+        
         // cout << a[i] << " ";
     }
-
+    
     reset_array(b, a, size);
     reset_array(c, a, size);
 }
 
 int main(int argc, const char * argv[]) {
-
+    
     float times[10][10];
     float timesmoy[10][10];
     int dim = 0;
-
+    
     clock_t t;
-
+    
     srand((unsigned) time(0));
-
-for(int cpt = 0; cpt < 10 ; cpt++){
-
-    for (int i = 1; i <= 10; i++) {
-
-        int a[i * SIZE], b[i * SIZE], c[i * SIZE];
-        int j = 0;
-        fill_array(a, b, c, i * SIZE);
-
-        t = clock();
-        MergeSort *sorter1 = new MergeSort(a, c, i * SIZE);
-        t = clock() - t;
-        times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
-        j++;
-        free(sorter1);
-        reset_array(b, a, i * SIZE);
-
-        t = clock();
-        BubbleSort *sorter2 = new BubbleSort(a, i * SIZE);
-        t = clock() - t;
-        times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
-        j++;
-        free(sorter2);
-        reset_array(b, a, i * SIZE);
-
-        t = clock();
-        QuickSort *sorter3 = new QuickSort(a, i * SIZE);
-        t = clock() - t;
-        times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
-        j++;
-        free(sorter3);
-        reset_array(b, a, i * SIZE);
-
-        // This one doesn't always work, I can't figure out the reason.
-        t = clock();
-        // QuickSortRand *sorter4 = new QuickSortRand(a, i * SIZE);
-        t = clock() - t;
-        times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
-        j++;
-        // free(sorter4);
-        // reset_array(b, a, i * SIZE);
-
-        t = clock();
-        InsertionSort *sorter4 = new InsertionSort(a, i * SIZE);
-        t = clock() - t;
-        times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
-        j++;
-        free(sorter4);
-        reset_array(b, a, i * SIZE);
-
-        t = clock();
-        HeapSort *sorter5 = new HeapSort(a, i * SIZE);
-        t = clock() - t;
-        times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
-        j++;
-        free(sorter5);
-        reset_array(b, a, i * SIZE);
-
-        t = clock();
-        RadixSort *sorter6 = new RadixSort(a, i * SIZE);
-        t = clock() - t;
-        times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
-        j++;
-        free(sorter6);
-        reset_array(b, a, i * SIZE);
-
-        t = clock();
-        sort(a, (a + (i * SIZE)));
-        t = clock() - t;
-        times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
-        j++;
-        reset_array(b, a, i * SIZE);
-
-        t = clock();
-        sort_heap(a, (a + (i * SIZE)));
-        t = clock() - t;
-        times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
-        j++;
-        reset_array(b, a, i * SIZE);
-
-        t = clock();
-        stable_sort(a, (a + (i * SIZE)));
-        t = clock() - t;
-        times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
-        j++;
-        reset_array(b, a, i * SIZE);
-
-    }
-
+    
+    for(int cpt = 0; cpt < 10 ; cpt++){
+        
+        for (int i = 1; i <= 10; i++) {
+            
+            int a[i * SIZE], b[i * SIZE], c[i * SIZE];
+            int j = 0;
+            fill_array(a, b, c, i * SIZE);
+            
+            t = clock();
+            MergeSort *sorter1 = new MergeSort(a, c, i * SIZE);
+            t = clock() - t;
+            times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
+            j++;
+            free(sorter1);
+            reset_array(b, a, i * SIZE);
+            
+            t = clock();
+            BubbleSort *sorter2 = new BubbleSort(a, i * SIZE);
+            t = clock() - t;
+            times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
+            j++;
+            free(sorter2);
+            reset_array(b, a, i * SIZE);
+            
+            t = clock();
+            QuickSort *sorter3 = new QuickSort(a, i * SIZE);
+            t = clock() - t;
+            times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
+            j++;
+            free(sorter3);
+            reset_array(b, a, i * SIZE);
+            
+            // This one doesn't always work, I can't figure out the reason.
+            t = clock();
+            // QuickSortRand *sorter4 = new QuickSortRand(a, i * SIZE);
+            t = clock() - t;
+            times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
+            j++;
+            // free(sorter4);
+            // reset_array(b, a, i * SIZE);
+            
+            t = clock();
+            InsertionSort *sorter4 = new InsertionSort(a, i * SIZE);
+            t = clock() - t;
+            times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
+            j++;
+            free(sorter4);
+            reset_array(b, a, i * SIZE);
+            
+            t = clock();
+            HeapSort *sorter5 = new HeapSort(a, i * SIZE);
+            t = clock() - t;
+            times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
+            j++;
+            free(sorter5);
+            reset_array(b, a, i * SIZE);
+            
+            t = clock();
+            RadixSort *sorter6 = new RadixSort(a, i * SIZE);
+            t = clock() - t;
+            times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
+            j++;
+            free(sorter6);
+            reset_array(b, a, i * SIZE);
+            
+            t = clock();
+            sort(a, (a + (i * SIZE)));
+            t = clock() - t;
+            times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
+            j++;
+            reset_array(b, a, i * SIZE);
+            
+            t = clock();
+            sort_heap(a, (a + (i * SIZE)));
+            t = clock() - t;
+            times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
+            j++;
+            reset_array(b, a, i * SIZE);
+            
+            t = clock();
+            stable_sort(a, (a + (i * SIZE)));
+            t = clock() - t;
+            times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
+            j++;
+            reset_array(b, a, i * SIZE);
+            
+        }
+        
         for (int i1 = 0; i1 < 10; i1++) {
             for (int j2 = 0; j2 < 10; j2++) {
-
-                if(cpt==0)
+                
+                if(cpt == 0)
                 {
                     timesmoy[i1][j2] = times[i1][j2];
-                }else{
+                }
+                else {
                     timesmoy[i1][j2] = (timesmoy[i1][j2] + times[i1][j2])/2 ;
                 }
             }
         }
-
-}
-
+        
+    }
+    
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-
-            printf("%d ", timesmoy[i][j]);
-
-
+            
+            printf("%f ", timesmoy[i][j]);
+            
         }
-
+        
         cout << endl << endl;
-
+        
     }
-
-
+    
+    
     cout << "Choisissez la dimension des matrices :" << endl;
     cin >> dim ;
-
-     Matrix m1(dim);
-     Matrix m2(dim);
-     cout << "Matrice A :\n" << m1 << endl;
-     cout << "Matrice B :\n" << m2 << endl;
-     Matrix result(4);
-     result = m1.strassen(m2);
-     cout << "Strassen de A * B:\n" << result << endl;
-
+    
+    Matrix m1(dim);
+    Matrix m2(dim);
+    cout << "Matrice A :\n" << m1 << endl;
+    cout << "Matrice B :\n" << m2 << endl;
+    Matrix result(4);
+    result = m1.strassen(m2);
+    cout << "Strassen de A * B:\n" << result << endl;
+    
     system("pause");
-
+    
     return 0;
 }
--- AlgoDevoir1/AlgoDevoir1/matrix.h
+++ AlgoDevoir1/AlgoDevoir1/matrix.h
@@ -2,6 +2,9 @@
 //  Matrix.h
 //  AlgoDevoir1
 //
+//  Created by Julie Brillion on 20/05/15.
+//  Copyright (c) 2015 Julie Brillion. All rights reserved.
+//
 
 #ifndef MATRIX_H_
 #define MATRIX_H_
@@ -12,47 +15,47 @@
 #include "time.h"
 
 class Matrix {
-	size_t width;
-	int *data;
+    size_t width;
+    int *data;
 public:
-	Matrix(size_t _width) : width(_width) {
-
-    int random = 0;
-	srand(time(NULL));
-
-		this->data = new int[_width * _width];
-
-		for (size_t i = 0; i<width * width; ++i) {
+    Matrix(size_t _width) : width(_width) {
+        
+        int random = 0;
+        srand(time(NULL));
+        
+        this->data = new int[_width * _width];
+        
+        for (size_t i = 0; i<width * width; ++i) {
             random =rand()%50;
-			this->data[i] = random;
-		}
-	}
-
-
-	Matrix(int *data, size_t _width) : Matrix(_width) {
-		for (size_t i = 0; i<width * width; ++i) {
-			this->data[i] = data[i];
-		}
-	}
-
-	virtual ~Matrix() {
-		if (this->data != NULL) {
-			delete[] this->data;
-			this->data = NULL;
-		}
-	}
-
-	size_t get_width() const{
-		return width;
-	}
-	Matrix & operator=(const Matrix & right);
-	Matrix operator+(const Matrix & right) const;
-	Matrix operator-(const Matrix & right) const;
-	Matrix operator*(const Matrix & right) const;
-	int & operator[](const size_t i);
-	const int & operator[](const size_t i) const;
-
-	Matrix strassen(const Matrix &b) const;
+            this->data[i] = random;
+        }
+    }
+    
+    
+    Matrix(int *data, size_t _width) : Matrix(_width) {
+        for (size_t i = 0; i<width * width; ++i) {
+            this->data[i] = data[i];
+        }
+    }
+    
+    virtual ~Matrix() {
+        if (this->data != NULL) {
+            delete[] this->data;
+            this->data = NULL;
+        }
+    }
+    
+    size_t get_width() const{
+        return width;
+    }
+    Matrix & operator=(const Matrix & right);
+    Matrix operator+(const Matrix & right) const;
+    Matrix operator-(const Matrix & right) const;
+    Matrix operator*(const Matrix & right) const;
+    int & operator[](const size_t i);
+    const int & operator[](const size_t i) const;
+    
+    Matrix strassen(const Matrix &b) const;
 };
 
 std::ostream & operator<<(std::ostream &os, Matrix &matrix);
