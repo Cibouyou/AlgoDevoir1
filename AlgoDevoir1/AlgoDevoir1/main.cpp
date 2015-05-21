d807c6dfcd63b8de2003f76269c9c716b65e82b3
Cibouyou <cibouyou@gmail.com>
2015-05-21 17:08:27 -0400

    10 iterations

--- AlgoDevoir1/AlgoDevoir1/main.cpp
+++ AlgoDevoir1/AlgoDevoir1/main.cpp
@@ -11,6 +11,8 @@
 #include <cstdlib>
 #include <ctime>
 #include <time.h>
+#include <math.h>
+#include <algorithm>
 #include "MergeSort.h"
 #include "BubbleSort.h"
 #include "QuickSort.h"
@@ -20,16 +22,17 @@
 #include "RadixSort.h"
 #include "Matrix.h"
 
+
 #define SIZE 100
 
 using namespace std;
 
 void reset_array(int *a, int *b, int size) {
-    
+
     for(int i = 0; i < size; i++) {
         a[i] = b[i];
     }
-    
+
 }
 
 void fill_array(int *a, int *b, int *c, int size) {
@@ -38,7 +41,7 @@ void fill_array(int *a, int *b, int *c, int size) {
 
         // cout << a[i] << " ";
     }
-    
+
     reset_array(b, a, size);
     reset_array(c, a, size);
 }
@@ -46,30 +49,21 @@ void fill_array(int *a, int *b, int *c, int size) {
 int main(int argc, const char * argv[]) {
 
     float times[10][10];
+    float timesmoy[10][10];
     int dim = 0;
 
     clock_t t;
 
     srand((unsigned) time(0));
 
-	int input1[4][4] = {
-			{ 13, 3, 6, 3 },
-			{ 6, 2, 8, 87 },
-			{ 37, 42, 57, 69 },
-			{ 42, 53, 64, 74 } };
+for(int cpt = 0; cpt < 10 ; cpt++){
 
-	int input2[4][4] = {
-			{ 75, 68, 5, 4 },
-			{ 64, 58, 47, 3 },
-			{ 55, 48, 34, 2 },
-			{ 45, 34, 27, 1 } };
-    
     for (int i = 1; i <= 10; i++) {
-            
+
         int a[i * SIZE], b[i * SIZE], c[i * SIZE];
         int j = 0;
         fill_array(a, b, c, i * SIZE);
-        
+
         t = clock();
         MergeSort *sorter1 = new MergeSort(a, c, i * SIZE);
         t = clock() - t;
@@ -77,7 +71,7 @@ int main(int argc, const char * argv[]) {
         j++;
         free(sorter1);
         reset_array(b, a, i * SIZE);
-        
+
         t = clock();
         BubbleSort *sorter2 = new BubbleSort(a, i * SIZE);
         t = clock() - t;
@@ -85,7 +79,7 @@ int main(int argc, const char * argv[]) {
         j++;
         free(sorter2);
         reset_array(b, a, i * SIZE);
-        
+
         t = clock();
         QuickSort *sorter3 = new QuickSort(a, i * SIZE);
         t = clock() - t;
@@ -93,7 +87,7 @@ int main(int argc, const char * argv[]) {
         j++;
         free(sorter3);
         reset_array(b, a, i * SIZE);
-        
+
         // This one doesn't always work, I can't figure out the reason.
         t = clock();
         // QuickSortRand *sorter4 = new QuickSortRand(a, i * SIZE);
@@ -102,7 +96,7 @@ int main(int argc, const char * argv[]) {
         j++;
         // free(sorter4);
         // reset_array(b, a, i * SIZE);
-        
+
         t = clock();
         InsertionSort *sorter4 = new InsertionSort(a, i * SIZE);
         t = clock() - t;
@@ -110,7 +104,7 @@ int main(int argc, const char * argv[]) {
         j++;
         free(sorter4);
         reset_array(b, a, i * SIZE);
-        
+
         t = clock();
         HeapSort *sorter5 = new HeapSort(a, i * SIZE);
         t = clock() - t;
@@ -118,7 +112,7 @@ int main(int argc, const char * argv[]) {
         j++;
         free(sorter5);
         reset_array(b, a, i * SIZE);
-        
+
         t = clock();
         RadixSort *sorter6 = new RadixSort(a, i * SIZE);
         t = clock() - t;
@@ -126,39 +120,54 @@ int main(int argc, const char * argv[]) {
         j++;
         free(sorter6);
         reset_array(b, a, i * SIZE);
-        
+
         t = clock();
         sort(a, (a + (i * SIZE)));
         t = clock() - t;
         times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
         j++;
         reset_array(b, a, i * SIZE);
-        
+
         t = clock();
         sort_heap(a, (a + (i * SIZE)));
         t = clock() - t;
         times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
         j++;
         reset_array(b, a, i * SIZE);
-        
+
         t = clock();
         stable_sort(a, (a + (i * SIZE)));
         t = clock() - t;
         times[i - 1][j] = ((float) t ) / CLOCKS_PER_SEC;
         j++;
         reset_array(b, a, i * SIZE);
-        
+
     }
-    
+
+        for (int i1 = 0; i1 < 10; i1++) {
+            for (int j2 = 0; j2 < 10; j2++) {
+
+                if(cpt==0)
+                {
+                    timesmoy[i1][j2] = times[i1][j2];
+                }else{
+                    timesmoy[i1][j2] = (timesmoy[i1][j2] + times[i1][j2])/2 ;
+                }
+            }
+        }
+
+}
+
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            
-            printf("%f ", times[i][j]);
-            
+
+            printf("%d ", timesmoy[i][j]);
+
+
         }
-        
+
         cout << endl << endl;
-        
+
     }
 
 
